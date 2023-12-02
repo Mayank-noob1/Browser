cf2412bf535423bd6aee9dfc6582fc2a70b03bd1
Mayank-noob1 <109759622+Mayank-noob1@users.noreply.github.com>
2023-12-02 23:36:44 +0530

    Closes implicit tags.

---
+++ Chapter4.py
@@ -0,0 +1,274 @@
+from Chapter1 import *
+import tkinter
+import tkinter.font
+
+WIDTH = 1400
+HEIGHT = 800
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
+weight_ = "normal"
+style_ = "roman"
+FONTS = {}
+
+class Text:
+    def __init__(self, text, parent):
+        self.text = text
+        self.children = []
+        self.parent = parent
+
+    def __repr__(self):
+        return repr(self.text)
+
+class Element:
+    def __init__(self, tag, attributes, parent):
+        self.tag = tag
+        self.attributes = attributes
+        self.children = []
+        self.parent = parent
+
+    def __repr__(self):
+        return "<" + self.tag + ">"
+
+def get_font(size, weight, slant):
+    key = (size, weight, slant)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=slant)
+        FONTS[key] = font
+    return FONTS[key]
+
+class Layout:
+    def __init__(self,tree):
+        self.display_list=[]
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 16
+        self.line = []
+        self.recurse(tree)
+        self.flush()
+
+    def flush(self):
+        if not self.line: return
+        metrics= [font.metrics() for x,word,font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        baseline = self.cursor_y + 1.25 * max_ascent
+
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+        
+        self.cursor_x = HSTEP
+        self.line = []
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent 
+
+    def word(self,word):
+        font = get_font(self.size, self.weight, self.style)    
+        w = font.measure(word)
+        if self.cursor_x + w > WIDTH - HSTEP:
+            self.flush()
+        self.line.append((self.cursor_x,word,font))
+        self.cursor_x += w + font.measure(" ")
+
+    def recurse(self, tree):
+        if isinstance(tree, Text):
+            for word in tree.text.split():
+                self.word(word)
+        else:
+            self.open_tag(tree.tag)
+            for child in tree.children:
+                self.recurse(child)
+            self.close_tag(tree.tag)
+
+    def open_tag(self, tag):
+        if tag == "i":
+            self.style = "italic"
+        elif tag == "b":
+            self.weight = "bold"
+        elif tag == "small":
+            self.size -= 2
+        elif tag == "big":
+            self.size += 4
+        elif tag == "br":
+            self.flush()
+
+    def close_tag(self, tag):
+        if tag == "i":
+            self.style = "roman"
+        elif tag == "b":
+            self.weight = "normal"
+        elif tag == "small":
+            self.size += 2
+        elif tag == "big":
+            self.size -= 4
+        elif tag == "p":
+            self.flush()
+            self.cursor_y += VSTEP 
+
+class HTMLParser:
+    SELF_CLOSING_TAGS = [
+    "area", "base", "br", "col", "embed", "hr", "img", "input",
+    "link", "meta", "param", "source", "track", "wbr",]
+    HEAD_TAGS = [
+        "base", "basefont", "bgsound", "noscript",
+        "link", "meta", "title", "style", "script",
+    ]
+    def __init__(self, body):
+        self.body = body
+        self.unfinished = []
+
+    def parse(self):
+        text = ""
+        in_tag = False
+        for c in self.body:
+            if c == "<":
+                in_tag = True
+                if text: self.add_text(text)
+                text = ""
+            elif c == ">":
+                in_tag = False
+                self.add_tag(text)
+                text = ""
+            else:
+                text += c
+        if not in_tag and text:
+            self.add_text(text)
+        return self.finish()
+    
+    def implicit_tag(self,tag):
+        while True:
+            open_tags = [node.tag for node in self.unfinished]
+            if open_tags == [] and tag != "html":
+                self.add_tag("html")
+            elif open_tags == ["html"] \
+                and tag not in ["head", "body", "/html"]:
+                if tag in self.HEAD_TAGS:
+                    self.add_tag("head")
+                else:
+                    self.add_tag("body")
+            elif open_tags == ["html", "head"] and \
+                tag not in ["/head"] + self.HEAD_TAGS:
+                self.add_tag("/head")
+            else: break
+
+    def add_text(self, text):
+        if (text.isspace()): return
+        self.implicit_tag(None)
+        parent = self.unfinished[-1]
+        node = Text(text, parent)
+        parent.children.append(node)
+
+    def add_tag(self,tag):
+        tag, attributes = self.get_attributes(tag)
+        if tag.startswith("!"): return
+        self.implicit_tag(tag)
+        if tag.startswith("/"):
+            if len(self.unfinished) == 1: return
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        elif tag in self.SELF_CLOSING_TAGS:
+            parent = self.unfinished[-1]
+            node = Element(tag,attributes, parent)
+            parent.children.append(node)
+        else:
+            parent = self.unfinished[-1] if self.unfinished else None
+            node = Element(tag,attributes, parent)
+            self.unfinished.append(node)
+
+    def finish(self):
+        if len(self.unfinished) == 0:
+            self.add_tag("html")
+        while len(self.unfinished) > 1:
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        return self.unfinished.pop()
+    
+    def get_attributes(self, text):
+        parts = text.split()
+        tag = parts[0].lower()
+        attributes = {}
+        for attrpair in parts[1:]:
+            if '=' in attrpair:
+                key,val = attrpair.split('=',1)
+                if len(val) > 2 and val[0] in ["'", "\""]:
+                    val = val[1:-1]
+                attributes[key.lower()] = val
+            else:
+                attributes[attrpair.lower()] = ""
+        return tag, attributes
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.title("Jad Browser")
+        self.scroll = 0
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT
+        )
+
+        self.window.bind("<Down>", self.scrolldown)
+        self.window.bind("<Up>", self.scrollup)
+        self.window.bind("<Configure>",self.resize)
+        self.window.bind("<MouseWheel>",self.onMouseScroll)
+
+        self.vbar = tkinter.Scrollbar(self.window,orient="vertical")
+        self.vbar.config(command=self.canvas.yview)
+        self.canvas.config(yscrollcommand=self.vbar.set)
+
+        self.vbar.pack(side="right",fill="y")
+        self.canvas.pack(side="left",fill="both",expand=1)
+
+        self.bi_times = tkinter.font.Font(
+            family="Times",
+            size=16,
+            weight="bold",
+            slant="italic",
+        )
+
+    def load(self, url):
+        headers, body = request(url)
+        self.source = body
+        self.tokens = HTMLParser(body).parse()
+        self.display_list = Layout(self.tokens).display_list
+        self.draw()
+
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c, f in self.display_list:
+            if y > self.scroll + HEIGHT: continue
+            if y + f.metrics("linespace") < self.scroll: continue
+            self.canvas.create_text(x, y-self.scroll, text=c,font=f,anchor="nw")
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+    def scrollup(self, e):
+        self.scroll -= SCROLL_STEP
+        self.draw()
+        
+    def resize(self,e):
+        global HEIGHT,WIDTH
+        HEIGHT = self.window.winfo_height()
+        WIDTH = self.window.winfo_width()
+        self.display_list = Layout(self.tokens).display_list
+        self.draw()
+
+    def onMouseScroll(self,event):
+        self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+        self.draw()
+
+def print_tree(node, indent=0):
+    print(" " * indent, node)
+    for child in node.children:
+        print_tree(child, indent + 2)
+
+if __name__ == "__main__":
+    import sys
+    Browser().load(sys.argv[1])
+    tkinter.mainloop()
