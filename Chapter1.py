8b728fdf4d1d33543a142c0b25a43b7490f7f466
Mayank-noob1 <109759622+Mayank-noob1@users.noreply.github.com>
2023-12-05 22:47:48 +0530

    Added URL class and relative url support

--- Chapter1.py
+++ Chapter1.py
@@ -1,58 +1,74 @@
 import socket
 import ssl
-from bs4 import BeautifulSoup
 
-def parse_url(url): 
-    scheme, url = url.split("://", 1) 
-    assert scheme in ["http","https"], "Unknown scheme {}".format(scheme)
-    if "/" not in url:
-        url = url + "/" 
-    host, path = url.split("/", 1) 
-    return (scheme, host, "/" + path)
+class URL:
+    def __init__(self,url):
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme in ["http", "https"]
 
-def request(url):
-    s = socket.socket(
-        family=socket.AF_INET,
-        type=socket.SOCK_STREAM,
-        proto=socket.IPPROTO_TCP,
-    )
-    scheme, host, path = parse_url(url)
-    port = 80 if (scheme == "http") else 443
-    if ':' in host:
-        host,port = host.split(':',1)
-        port = int(port)
+        if "/" not in url:
+            url = url + "/"
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
 
-    s.connect((host, port))
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
 
-    if (scheme == "https"):
-        s = ssl.wrap_socket(s, keyfile=None, certfile=None, server_side=False, cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
+
+    def request(self):
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
+        )
+
+        s.connect((self.host,self.port))
+
+        if (self.scheme == "https"):
+            s = ssl.wrap_socket(s, keyfile=None, certfile=None, server_side=False, cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
+            
+        s.send("GET {} HTTP/1.0\r\n".format(self.path).encode("utf8") + 
+            "Host: {}\r\n\r\n".format(self.host).encode("utf8"))
+
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
+
+        headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n": break
+            header, value = line.split(":", 1)
+            headers[header.lower()] = value.strip()
+
+        body = response.read()
+        s.close()
+        return headers,body
         
-    s.send("GET {} HTTP/1.0\r\n".format(path).encode("utf8") + 
-        "Host: {}\r\n\r\n".format(host).encode("utf8"))
-
-    response = s.makefile("r", encoding="utf8", newline="\r\n")
-
-    statusline = response.readline()
-    version, status, explanation = statusline.split(" ", 2)
-
-    headers = {}
-    while True:
-        line = response.readline()
-        if line == "\r\n": break
-        header, value = line.split(":", 1)
-        headers[header.lower()] = value.strip()
-
-    body = response.read()
-    s.close()
-    return headers,body
-    
-def load(url):
-    headers, body = request(url)
-    # print(headers)
-    body = BeautifulSoup(body,"html.parser").prettify()
-    print(body)
-    return (headers,body)
+    def resolve(self, url):
+        if "://" in url: return URL(url)
+        if not url.startswith("/"):
+            dir, _ = self.path.rsplit("/", 1)
+            while url.startswith("../"):
+                _, url = url.split("/", 1)
+                if "/" in dir:
+                    dir, _ = dir.rsplit("/", 1)
+            url = dir + "/" + url
+        if url.startswith("//"):
+            return URL(self.scheme + ":" + url)
+        else:
+            return URL(self.scheme + "://" + self.host + \
+                       ":" + str(self.port) + url)
+    def load(self):
+        headers, body = self.request()
+        return (headers,body)
 
 if __name__ == "__main__":
     import sys
-    load(sys.argv[1])
+    URL(sys.argv[1]).load()
